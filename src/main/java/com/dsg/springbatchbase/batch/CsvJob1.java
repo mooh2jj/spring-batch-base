a2c5dc960e65eaa0bc84bd2d84a423f808306c22
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-05-25 22:10:23 +0900

    CSV Reader / Writer 1

---
+++ src/main/java/com/dsg/springbatchbase/batch/CsvJob1.java
@@ -0,0 +1,65 @@
+package com.dsg.springbatchbase.batch;
+
+import com.dsg.springbatchbase.dto.TwoDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class CsvJob1 {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private static final int chunkSize = 5;
+
+    @Bean
+    public Job csvJob11() {
+        return jobBuilderFactory.get("csvJob11")
+                .start(csvStep1())
+                .build();
+    }
+
+    @Bean
+    public Step csvStep1() {
+        return stepBuilderFactory.get("csvStep1")
+                .<TwoDto, TwoDto>chunk(chunkSize)
+                .reader(csvJob1_FileReader())
+                .writer(twoDto -> twoDto.stream().forEach(twoDto2 -> {
+                    log.info(twoDto2.toString());
+                })).build();
+    }
+
+    @Bean
+    public FlatFileItemReader<TwoDto> csvJob1_FileReader() {
+        FlatFileItemReader<TwoDto> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(new ClassPathResource("/sample/csvJob1_input.csv"));
+        flatFileItemReader.setLinesToSkip(1);
+
+        DefaultLineMapper<TwoDto> dtoDefaultLineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+        delimitedLineTokenizer.setNames("one", "two");
+        delimitedLineTokenizer.setDelimiter(":");
+
+        BeanWrapperFieldSetMapper<TwoDto> beanWrapperFieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        beanWrapperFieldSetMapper.setTargetType(TwoDto.class);
+
+        dtoDefaultLineMapper.setLineTokenizer(delimitedLineTokenizer);
+        dtoDefaultLineMapper.setFieldSetMapper(beanWrapperFieldSetMapper);
+        flatFileItemReader.setLineMapper(dtoDefaultLineMapper);
+
+        return flatFileItemReader;
+    }
+}
